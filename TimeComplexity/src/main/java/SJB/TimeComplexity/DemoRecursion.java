c11400d9928882b44e640550eee3f6d49a2b952d
Tamilmani027 <manims2727@gmail.com>
2025-07-10 14:12:53 +0530

    recursion

--- TimeComplexity/src/main/java/SJB/TimeComplexity/DemoRecursion.java
+++ TimeComplexity/src/main/java/SJB/TimeComplexity/DemoRecursion.java
@@ -1,15 +1,11 @@
 package SJB.TimeComplexity;
 
 public class DemoRecursion {
-
-    // Method to calculate factorial using recursion
     public static int fact(int n) {
         if (n == 0 || n == 1)
             return 1;
         return n * fact(n - 1);
     }
-
-    // Method to calculate nth Fibonacci number using recursion
     public static int fibo(int a) {
         if (a == 0)
             return 0;
@@ -21,11 +17,7 @@ public class DemoRecursion {
 
     public static void main(String[] args) {
         int n = 10;
-
-        // Display factorial of 5
         System.out.println("Factorial of 5 is: " + fact(5));
-
-        // Display Fibonacci series up to n terms
         System.out.print("Fibonacci Series up to " + n + " terms: ");
         for (int i = 0; i < n; i++) {
             System.out.print(fibo(i) + " ");
